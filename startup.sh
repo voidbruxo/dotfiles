b70e955266885c717e163b14901917847eeb9197
Emir Eary <imbruxo@noreply.codeberg.org>
2023-12-29 17:04:20 +0000

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-/usr/bin/picom --config ~/.config/picom/picom.conf &
-/usr/bin/nitrogen --restore &
-/usr/bin/pipewire &
-/usr/bin/pipewire-pulse &
-/usr/bin/wireplumber &
-/usr/bin/lxqt-policykit-agent &
