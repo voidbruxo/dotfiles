8ce1de78086f761a1e8a86dc645622e389f5a4c7
Emir Eary <imbruxo@noreply.codeberg.org>
2024-03-06 10:01:24 +0000

    Upload files to ".dwm"

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 /usr/bin/lxpolkit &
 slstatus &
 xscreensaver -nosplash &
-feh --bg-tile /home/geek/Pictures/wall/carv.png &
+feh --bg-tile /home/geek/Pictures/wall/space2.png &
 #/usr/bin/emacs --daemon &
 /usr/bin/picom --config /home/geek/.config/picom/picom.conf &
 /usr/bin/pipewire &
