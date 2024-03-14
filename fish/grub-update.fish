32340d33dbb4c9cd879df0dfa51ea3759226dec9
Emir Eary <imbruxo@noreply.codeberg.org>
2024-03-14 08:29:35 +0000

    Delete fish/grub-update.fish

--- fish/grub-update.fish
+++
@@ -1,4 +0,0 @@
-function grub-update --wraps='doas grub-mkconfig -o /boot/grub/grub.cfg' --description 'alias grub-update=doas grub-mkconfig -o /boot/grub/grub.cfg'
-  doas grub-mkconfig -o /boot/grub/grub.cfg $argv
-        
-end
