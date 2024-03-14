1f9496cb30a170e0427bb7580ee019b88fdf9cdc
Emir Eary <imbruxo@noreply.codeberg.org>
2024-03-14 08:29:44 +0000

    Delete fish/sys-time.fish

--- fish/sys-time.fish
+++
@@ -1,4 +0,0 @@
-function sys-time --wraps=stat\ /\ \|\ awk\ \'/Birth:\ /\{print\ \}\' --description alias\ sys-time=stat\ /\ \|\ awk\ \'/Birth:\ /\{print\ \}\'
-  stat / | awk '/Birth: /{print }' $argv
-        
-end
