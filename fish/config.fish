a12e9596c5b2b1c0606328a6d25a4a64500564a7
imbruxo <93203496+voidbruxo@users.noreply.github.com>
2024-04-21 06:45:17 +0000

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
-set EDITOR nvim
+set EDITOR "nvim"
+set TERM "xterm-256color"
+fish_add_path ~/.local/bin/
