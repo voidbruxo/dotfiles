b446da918c3250d0c67b6213258bdb1c215a6e52
imbruxo <93203496+voidbruxo@users.noreply.github.com>
2024-05-19 14:50:41 +0000

    Create status.sh

---
+++ sway/status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while true; do
+  # Get brightness percentage
+  brightness=$(brightnessctl g | awk '{print int($1/$(brightnessctl m)*100)}')
+  # Get volume percentage
+  volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -n1)
+  # Get date and time
+  date_time=$(date "+%Y-%m-%d")
+  # Icons (replace with the correct Unicode code points for your chosen Nerd Font)
+  vol_icon="" # FontAwesome volume icon
+  brightness_icon="󰃠" # FontAwesome sun icon
+  date_icon="" # FontAwesome calendar icon
+  time_icon="" # FontAwesome clock icon
+  # Output the status line with icons
+  echo "${brightness_icon} ${brightness}% | ${vol_icon} ${volume} | ${date_icon} ${date_time} | ${time_icon} $(date '+%H:%M')"
+  # Sleep for 1 second before updating again
+  sleep 1
+done
