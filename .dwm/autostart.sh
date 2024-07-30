95ed300e5ba2512a263bd79427637a12170e4428
imbruxo <93203496+voidbruxo@users.noreply.github.com>
2024-07-30 08:55:30 +0000

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-lxpolkit &
-lxclipboard &
 slstatus &
 ~/.config/wallp.sh &
-xscreensaver -nosplash &
-#picom &
-#pipewire &
-#pipewire-pulse &
-#wireplumber &
-/usr/bin/gentoo-pipewire-launcher
+picom &
+pkttyagent
