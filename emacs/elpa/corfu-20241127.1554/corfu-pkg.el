32295ef4bef5ab584371144c93a2f9a21b609d27
voidbruxo <emirerox@gmail.com>
2024-12-07 21:40:13 +0330

    Smile bro, I'm Getting better

---
+++ emacs/elpa/corfu-20241127.1554/corfu-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "corfu" "20241127.1554"
+  "COmpletion in Region FUnction."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/minad/corfu"
+  :commit "f10f58f446e2cbda738ae8738cf4a7464a8aeeab"
+  :revdesc "f10f58f446e2"
+  :keywords '("abbrev" "convenience" "matching" "completion" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
---
+++ emacs/elpa/evil-collection-20241201.1942/evil-collection-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "evil-collection" "20241201.1942"
+  "A set of keybindings for Evil mode."
+  '((emacs    "26.3")
+    (evil     "1.2.13")
+    (annalist "1.0"))
+  :url "https://github.com/emacs-evil/evil-collection"
+  :commit "ed8c652b1f49e3b01d787bf33ccb2345399572d4"
+  :revdesc "ed8c652b1f49"
+  :keywords '("evil" "tools")
+  :authors '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers '(("James Nguyen" . "james@jojojames.com")))
